dfcb967f8f974cea5f972b5c19779865dc131229
kaiqiang cao <kevinswiftios@gmail.com>
2019-03-23 11:17:31 +0800

    增加restaurant文件

--- app.js
+++ app.js
@@ -56,7 +56,8 @@ var myShowRouter = require('./routes/myShow');   // 万能路由
 var app = express();
 
 var restaurantRank = require('./routes/HomePage/RestaurantRank');  // 餐饮排行
-
+var qdhSpotCommentTotal = require('./routes/HomePage/QdhCommentTotal'); //千岛湖同环比
+var spotRank = require('./routes/HomePage/SpotRank'); //景区排行
 //qdhhoteltmapnumshow
 //日志文件的配置
 var log4js = require('log4js');
@@ -130,7 +131,9 @@ app.use('/api/shoparea/score', AreaScore);
 app.use('/api/shoplocation', shoplocation);
 
 app.use('/api/homepage/restaurantStatistical', restaurantRank);  // 餐饮排行
-// app.use('/api/homepage/restaurantColum', restaurantRank);  // 餐饮柱状图
+app.use('/api/homepage/spotrank', spotRank);  // 景区排行
+app.use('/api/homepage/qdhspotcommenttotal', qdhSpotCommentTotal);  // 景区排行
+
 // app.use('./api/homepage/restaurant')
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
